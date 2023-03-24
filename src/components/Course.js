b3e6c0d8e466d55221773c54369c3ab5425aa674
William-95 <verga.william.95@gmail.com>
2023-03-24 19:36:58 +0100

    a

--- src/components/Course.js
+++ src/components/Course.js
@@ -4,23 +4,52 @@ import clicker from "../img/course/clicker.jpg";
 
 export default function Course() {
   return (
-    <div>
+    <div className="section">
       <h3>Affiliate Course</h3>
-
-      <div className="cardCourse">
-        <h4>Clicker Training</h4>
-        <img src={clicker} alt="" />
-        <button className="primaryBtn">
-          <a href="https://canecampione.com/corso-istruttore-clicker-training/?gclid=EAIaIQobChMInLeyj8vy_QIVT-3tCh1wnQ-OEAAYASAAEgLWCvD_BwE">
-            More
+     
+      <div className="container">
+       
+        <div className="cardCourse">
+          <h4>Clicker Training</h4>
+          <img src={clicker} alt="" />
+          <button className="secondaryBtn">
+            <a href="https://canecampione.com/corso-istruttore-clicker-training/?gclid=EAIaIQobChMInLeyj8vy_QIVT-3tCh1wnQ-OEAAYASAAEgLWCvD_BwE">
+              More
             </a>
-        </button>
-        <p>
-        Clicker training is an active experience 
-        in which subjects, owners and dogs, 
-        collaborate with each other as in a 
-        couple dance
-        </p>
+          </button>
+          <p>
+            Clicker training is an active experience in which subjects, owners
+            and dogs, collaborate with each other as in a couple dance
+          </p>
+        </div>
+
+        <div className="cardCourse">
+          <h4>Clicker Training</h4>
+          <img src={clicker} alt="" />
+          <button className="secondaryBtn">
+            <a href="https://canecampione.com/corso-istruttore-clicker-training/?gclid=EAIaIQobChMInLeyj8vy_QIVT-3tCh1wnQ-OEAAYASAAEgLWCvD_BwE">
+              More
+            </a>
+          </button>
+          <p>
+            Clicker training is an active experience in which subjects, owners
+            and dogs, collaborate with each other as in a couple dance
+          </p>
+        </div>
+
+        <div className="cardCourse">
+          <h4>Clicker Training</h4>
+          <img src={clicker} alt="" />
+          <button className="secondaryBtn">
+            <a href="https://canecampione.com/corso-istruttore-clicker-training/?gclid=EAIaIQobChMInLeyj8vy_QIVT-3tCh1wnQ-OEAAYASAAEgLWCvD_BwE">
+              More
+            </a>
+          </button>
+          <p>
+            Clicker training is an active experience in which subjects, owners
+            and dogs, collaborate with each other as in a couple dance
+          </p>
+        </div>
       </div>
     </div>
   );
