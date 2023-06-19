2cdb326afd50754c376633d247c29c4bbffbb111
William-95 <verga.william.95@gmail.com>
2023-06-19 19:59:23 +0200

    Update CreateDogApi.js

--- src/api/CreateDogApi.js
+++ src/api/CreateDogApi.js
@@ -6,7 +6,7 @@ export default function CreateDogApi({ dati }) {
   const { api, send, setSend,setLoading,dog,setDog } = useContext(StateContext);
   const ref = useRef();
   const history = useHistory();
-
+const newDog=[...dog];
   /*requestApi*/
   useEffect(() => {
     if (ref.current !== send) {
@@ -23,7 +23,7 @@ export default function CreateDogApi({ dati }) {
            
             alert("Canne Inserito Correttamente.");
             const data = result.data;
-            setDog(dog=>[...dog,data]);
+            setDog(newDog=>[...newDog,data]);
             setLoading(false);
             history.push(`/singledog/${data.id}`);
 
