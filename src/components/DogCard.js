dbc670930930f328d642f3dc635e899b891e2548
William-95 <verga.william.95@gmail.com>
2023-06-21 22:34:04 +0200

    Update DogCard.js

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -22,8 +22,8 @@ export default function DogCard({ img, id, name }) {
 }
 
 DogCard.propTypes = {
-  name: PropTypes.string,
-  id: PropTypes.number,
-  img: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  img: PropTypes.string.isRequired,
 };
 // .isRequired
